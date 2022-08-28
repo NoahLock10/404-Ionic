5e0f38804112afe7103577ef55127d9ed8c6b3d3
Noah Lockhart <noahlock@tamu.edu>
2022-08-28 11:25:36 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'the12thman',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
